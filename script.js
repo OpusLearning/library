d3cecc5d800819bad4721195509e650d683b9b89
OpusLearning <James@opuslearning.co.uk>
2024-09-13 19:19:35 +0100

    refactored to class based implementation

--- script.js
+++ script.js
@@ -1,151 +1,173 @@
-let myLibrary = [];
-
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.id = Date.now(); // Add this line
-  this.info = function () {
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = Date.now(); // Add this line
+  }
+  info() {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${
       this.read ? "read" : "not read yet"
     }`;
-  };
+  }
 }
 
-function addBookToLibrary(title, author, pages, read) {
-  const newBook = new Book(title, author, pages, read);
-  newBook.id = Date.now(); // Use timestamp as a simple unique id
-  myLibrary.push(newBook);
-  saveBooks();
-}
-
-function createBookCard(book) {
-  const template = document.getElementById("book-card-template");
-  const bookCard = template.content.cloneNode(true).firstElementChild;
-
-  const titleElement = bookCard.querySelector(".book-title");
-  const authorElement = bookCard.querySelector(".book-author");
-  const pagesElement = bookCard.querySelector(".book-pages");
-  const readElement = bookCard.querySelector(".book-read");
-
-  if (titleElement) titleElement.textContent = book.title;
-  if (authorElement) authorElement.textContent = `Author: ${book.author}`;
-  if (pagesElement) pagesElement.textContent = `Pages: ${book.pages}`;
-  if (readElement)
-    readElement.textContent = book.read ? "Read" : "Not read yet";
-
-  const menuButton = bookCard.querySelector(".menu-button");
-  if (menuButton) {
-    menuButton.setAttribute("data-book-id", book.id); // Ensure each book has a unique id
+class Library {
+  constructor() {
+    this.books = [];
+  }
+  addBook(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+    newBook.id = Date.now();
+    this.books.push(newBook);
+    this.saveBooks();
   }
 
-  const toggleReadLink = bookCard.querySelector(".toggle-read");
-  if (toggleReadLink) {
-    toggleReadLink.addEventListener("click", (e) => {
-      e.preventDefault();
+  removeBook(id) {
+    const index = this.books.findIndex((book) => book.id === id);
+    if (index > -1) {
+      this.books.splice(index, 1);
+      this.saveBooks();
+    }
+  }
+
+  toggleReadStatus(id) {
+    const book = this.books.find((book) => book.id === id);
+    if (book) {
       book.read = !book.read;
-      if (readElement)
-        readElement.textContent = book.read ? "Read" : "Not read yet";
-      saveBooks();
-    });
+      this.saveBooks();
+    }
   }
 
-  const deleteLink = bookCard.querySelector(".delete-book");
-  if (deleteLink) {
-    deleteLink.addEventListener("click", (e) => {
-      e.preventDefault();
-      const index = myLibrary.indexOf(book);
-      if (index > -1) {
-        myLibrary.splice(index, 1);
-        saveBooks();
-        displayBooks();
+  saveBooks() {
+    localStorage.setItem("books", JSON.stringify(this.books));
+  }
+
+  loadBooks() {
+    const books = JSON.parse(localStorage.getItem("books")) || [];
+    this.books = books.map(
+      (book) => new Book(book.title, book.author, book.pages, book.read)
+    );
+  }
+}
+
+class UI {
+  constructor(library) {
+    this.library = library;
+    this.bookList = document.getElementById("bookList");
+    this.modal = document.getElementById("myModal");
+    this.bookForm = document.getElementById("bookForm");
+    this.addBookBtn = document.getElementById("myBtn");
+    this.closeModalBtn = document.getElementsByClassName("close")[0];
+    this.setupEventListeners();
+  }
+
+  setupEventListeners() {
+    document.addEventListener("click", (e) => this.handleGlobalClick(e));
+    this.bookForm.addEventListener("submit", (e) => this.handleFormSubmit(e));
+    this.addBookBtn.addEventListener("click", () => this.openModal());
+    this.closeModalBtn.addEventListener("click", () => this.closeModal());
+    window.addEventListener("click", (event) => {
+      if (event.target === this.modal) {
+        this.closeModal();
       }
     });
   }
 
-  return bookCard;
-}
+  createBookCard(book) {
+    const template = document.getElementById("book-card-template");
+    const bookCard = template.content.cloneNode(true).firstElementChild;
 
-function displayBooks() {
-  const bookList = document.getElementById("bookList");
-  bookList.innerHTML = "";
-  for (let book of myLibrary) {
-    const bookCard = createBookCard(book);
-    bookList.appendChild(bookCard);
+    const titleElement = bookCard.querySelector(".book-title");
+    const authorElement = bookCard.querySelector(".book-author");
+    const pagesElement = bookCard.querySelector(".book-pages");
+    const readElement = bookCard.querySelector(".book-read");
+
+    if (titleElement) titleElement.textContent = book.title;
+    if (authorElement) authorElement.textContent = `Author: ${book.author}`;
+    if (pagesElement) pagesElement.textContent = `Pages: ${book.pages}`;
+    if (readElement)
+      readElement.textContent = book.read ? "Read" : "Not read yet";
+
+    const menuButton = bookCard.querySelector(".menu-button");
+    if (menuButton) {
+      menuButton.setAttribute("data-book-id", book.id);
+    }
+
+    const toggleReadLink = bookCard.querySelector(".toggle-read");
+    if (toggleReadLink) {
+      toggleReadLink.addEventListener("click", (e) => {
+        e.preventDefault();
+        this.library.toggleReadStatus(book.id);
+        this.displayBooks();
+      });
+    }
+
+    const deleteLink = bookCard.querySelector(".delete-book");
+    if (deleteLink) {
+      deleteLink.addEventListener("click", (e) => {
+        e.preventDefault();
+        this.library.removeBook(book.id);
+        this.displayBooks();
+      });
+    }
+
+    return bookCard;
   }
-}
 
-// Global event listener for menu buttons
-document.addEventListener("click", (e) => {
-  if (e.target.classList.contains("menu-button")) {
-    e.stopPropagation();
-    const menuContent = e.target.nextElementSibling;
+  displayBooks() {
+    this.bookList.innerHTML = "";
+    for (let book of this.library.books) {
+      const bookCard = this.createBookCard(book);
+      this.bookList.appendChild(bookCard);
+    }
+  }
 
-    // Close all other open menus
-    document.querySelectorAll(".menu-content.show").forEach((menu) => {
-      if (menu !== menuContent) {
+  handleGlobalClick(e) {
+    if (e.target.classList.contains("menu-button")) {
+      e.stopPropagation();
+      const menuContent = e.target.nextElementSibling;
+
+      document.querySelectorAll(".menu-content.show").forEach((menu) => {
+        if (menu !== menuContent) {
+          menu.classList.remove("show");
+        }
+      });
+
+      menuContent.classList.toggle("show");
+    } else if (!e.target.closest(".menu-content")) {
+      document.querySelectorAll(".menu-content.show").forEach((menu) => {
         menu.classList.remove("show");
-      }
-    });
-
-    // Toggle the clicked menu
-    menuContent.classList.toggle("show");
-  } else if (!e.target.closest(".menu-content")) {
-    // Close all menus when clicking outside
-    document.querySelectorAll(".menu-content.show").forEach((menu) => {
-      menu.classList.remove("show");
-    });
+      });
+    }
   }
-});
 
-const modal = document.getElementById("myModal");
-const btn = document.getElementById("myBtn");
-const span = document.getElementsByClassName("close")[0];
+  handleFormSubmit(e) {
+    e.preventDefault();
+    const form = e.target;
+    const title = form.elements["title"].value;
+    const author = form.elements["author"].value;
+    const pages = form.elements["pages"].value;
+    const read = form.elements["read"].checked;
 
-btn.onclick = function () {
-  modal.style.display = "block";
-};
-
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+    this.library.addBook(title, author, pages, read);
+    form.reset();
+    this.closeModal();
+    this.displayBooks();
   }
-};
 
-const bookForm = document.getElementById("bookForm");
-bookForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-  const title = this.elements["title"].value;
-  const author = this.elements["author"].value;
-  const pages = this.elements["pages"].value;
-  const read = this.elements["read"].checked;
+  openModal() {
+    this.modal.style.display = "block";
+  }
 
-  addBookToLibrary(title, author, pages, read);
-  this.reset();
-  modal.style.display = "none";
-  displayBooks();
-});
-
-displayBooks();
-
-// Make sure this function is defined
-function saveBooks() {
-  localStorage.setItem("books", JSON.stringify(myLibrary));
+  closeModal() {
+    this.modal.style.display = "none";
+  }
 }
 
-// Make sure to load books when the page loads
-function loadBooks() {
-  const books = JSON.parse(localStorage.getItem("books")) || [];
-  myLibrary = books.map(
-    (book) => new Book(book.title, book.author, book.pages, book.read)
-  );
-}
+const library = new Library();
+const ui = new UI(library);
 
-// Call loadBooks and displayBooks when the page loads
-loadBooks();
-displayBooks();
+library.loadBooks();
+ui.displayBooks();
